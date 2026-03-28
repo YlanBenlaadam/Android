7551d1ac7ad92cb34cbbc23b9ea8e38c093ff87d
YlanBenlaadam <ylanbenlaadammailpro@gmail.com>
2026-03-28 10:16:55 +0100

    finalisation du tp7 ajout btn perso land de pizza fragement et correction des doublons dans ingrédientsfragement

--- app/src/main/java/com/example/pizzeria/IngredientsFragment.java
+++ app/src/main/java/com/example/pizzeria/IngredientsFragment.java
@@ -21,6 +21,9 @@ public class IngredientsFragment extends Fragment implements View.OnClickListene
     private Button BjambonCru;
     private Button BjambonCuit;
     private Button Bvalider;
+    //permettra de save les ingredients app rotation
+    private static final String CLE_SAVE_INGREDIENTS = "CLE_SAVE_INGREDIENTS";
+
 
     private ArrayList<String> ingredientsSelectionnes = new ArrayList<>();
 
@@ -54,50 +57,102 @@ public class IngredientsFragment extends Fragment implements View.OnClickListene
         BjambonCuit.setOnClickListener(this);
         Bvalider.setOnClickListener(this);
 
+        // on verifia que ce n'est pas le premier lancement du fagement
+        if (savedInstanceState != null) {
+            ingredientsSelectionnes = savedInstanceState.getStringArrayList(CLE_SAVE_INGREDIENTS);
+            // on verifie si l'ingredient etait present dans les ingredients selectionner
+            if (ingredientsSelectionnes.contains("mozzarella")) {
+                Bmozzarella.setText("MOZZARELLA ✓");// si c le cas on maj son btn99
+            }
+            if (ingredientsSelectionnes.contains("gorgonzola")) {
+                Bgorgonzola.setText("GORGONZOLA ✓");
+            }
+            if (ingredientsSelectionnes.contains("anchois")) {
+                Bancchois.setText("ANCHOIS ✓");
+            }
+            if (ingredientsSelectionnes.contains("capres")) {
+                Bcapres.setText("CAPRES ✓");
+            }
+            if (ingredientsSelectionnes.contains("olives")) {
+                Bolives.setText("OLIVES ✓");
+            }
+            if (ingredientsSelectionnes.contains("artichauts")) {
+                Bartichauts.setText("ARTICHAUTS ✓");
+            }
+            if (ingredientsSelectionnes.contains("jambon cru")) {
+                BjambonCru.setText("JAMBON CRU ✓");
+            }
+            if (ingredientsSelectionnes.contains("jambon cuit")) {
+                BjambonCuit.setText("JAMBON CUIT ✓");
+            }
+        }
+
+
         return v;
     }
 
     public void onClick(View v) {
-
+    /* ici on ajoute une verif supplementaire, pr eviter les doublons
+        le clique fonctionne seulement si l'ingredient n'a pas deja ete selectionner
+         if (!ingredientsSelectionnes.contains("ingredient"))
+     */
         if (v.getId() == R.id.Mozzarella) {
-            ingredientsSelectionnes.add("mozzarella");
-            Bmozzarella.setText("MOZZARELLA ✓");
+            if (!ingredientsSelectionnes.contains("mozzarella")) {
+                ingredientsSelectionnes.add("mozzarella");
+                Bmozzarella.setText("MOZZARELLA ✓");
+            }
         }
 
+
         if (v.getId() == R.id.Gorgonzola) {
-            ingredientsSelectionnes.add("gorgonzola");
-            Bgorgonzola.setText("GORGONZOLA ✓");
+            if (!ingredientsSelectionnes.contains("gorgonzola")) {
+                ingredientsSelectionnes.add("gorgonzola");
+                Bgorgonzola.setText("GORGONZOLA ✓");
+            }
         }
 
         if (v.getId() == R.id.Anchois) {
-            ingredientsSelectionnes.add("anchois");
-            Bancchois.setText("ANCHOIS ✓");
+            if (!ingredientsSelectionnes.contains("anchois")) {
+                ingredientsSelectionnes.add("anchois");
+                Bancchois.setText("ANCHOIS ✓");
+            }
         }
 
         if (v.getId() == R.id.Capres) {
-            ingredientsSelectionnes.add("capres");
-            Bcapres.setText("CAPRES ✓");
+            if (!ingredientsSelectionnes.contains("capres")) {
+                ingredientsSelectionnes.add("capres");
+                Bcapres.setText("CAPRES ✓");
+            }
         }
 
         if (v.getId() == R.id.Olives) {
-            ingredientsSelectionnes.add("olives");
-            Bolives.setText("OLIVES ✓");
+            if (!ingredientsSelectionnes.contains("olives")) {
+                ingredientsSelectionnes.add("olives");
+                Bolives.setText("OLIVES ✓");
+            }
         }
 
         if (v.getId() == R.id.Artichauts) {
-            ingredientsSelectionnes.add("artichauts");
-            Bartichauts.setText("ARTICHAUTS ✓");
+            if (!ingredientsSelectionnes.contains("artichauts")) {
+                ingredientsSelectionnes.add("artichauts");
+                Bartichauts.setText("ARTICHAUTS ✓");
+            }
         }
 
         if (v.getId() == R.id.JambonCru) {
-            ingredientsSelectionnes.add("jambon cru");
-            BjambonCru.setText("JAMBON CRU ✓");
+            if (!ingredientsSelectionnes.contains("jambon cru")) {
+                ingredientsSelectionnes.add("jambon cru");
+                BjambonCru.setText("JAMBON CRU ✓");
+            }
         }
 
         if (v.getId() == R.id.JambonCuit) {
-            ingredientsSelectionnes.add("jambon cuit");
-            BjambonCuit.setText("JAMBON CUIT ✓");
+            if (!ingredientsSelectionnes.contains("jambon cuit")) {
+                ingredientsSelectionnes.add("jambon cuit");
+                BjambonCuit.setText("JAMBON CUIT ✓");
+            }
         }
+
 
         if (v.getId() == R.id.Valider) {
             MainActivity activity = (MainActivity) getActivity();
@@ -108,4 +163,11 @@ public class IngredientsFragment extends Fragment implements View.OnClickListene
             }
         }
     }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putStringArrayList(CLE_SAVE_INGREDIENTS, ingredientsSelectionnes);
+    }
+
 }
